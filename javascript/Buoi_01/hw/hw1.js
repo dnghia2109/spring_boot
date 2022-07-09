5870601eaf0fb5140f444b311d6d1464ae8cfdf5
Lai Duy Nghia <laiduynghia21@gmail.com>
2022-07-09 15:50:53 +0700

    hw js01

--- javascript/Buoi_01/hw/hw1.js
+++ javascript/Buoi_01/hw/hw1.js
@@ -1,17 +1,91 @@
 function calculateFactorial(num) {
-	if (num = 1) {
-        return 1;
-    }
-    return num * calculateFactorial(num - 1);
+  if (num == 1) {
+    return 1;
+  }
+  return num * calculateFactorial(num - 1);
 }
 console.log(calculateFactorial(3));
 
 // Câu 2:
 function reverseStringname(text) {
-    let reverseString = "" ;
-    for (let i = text.length - 1; i >= 0; i--) {
-        reverseString += text[i];
-    }
-    return reverseString;
+  let reverseString = "";
+  for (let i = text.length - 1; i >= 0; i--) {
+    reverseString += text[i];
+  }
+  return reverseString;
 }
 console.log(reverseStringname("hello"));
+
+// Câu 3:
+function translateCode(code) {
+  switch (code) {
+    case "VN":
+      console.log("Viet Nam");
+      break;
+
+    case "US":
+      console.log("United State");
+      break;
+
+    case "ENG":
+      console.log("England");
+      break;
+
+    default:
+      console.log("Alo");
+      break;
+  }
+}
+translateCode("VN");
+
+// Câu 4:
+function subString(text) {
+    let newSubString = "";
+    for (let i = 0; i < 10; i++) {
+        newSubString += text[i] ;
+    }
+    return newSubString + "...";
+}
+console.log(subString("ToiNayTroiKhongMua"));
+
+// Câu 5:
+function keoBuaBao(yourAction){
+    let action = ["keo","bua","bao"];
+    let turn = Math.floor(Math.random() *3);
+    // console.log(turn);
+    if(yourAction == action[turn]) {
+        return "DRAW";
+    }
+
+    if(yourAction == "keo"){
+        if(action[turn] == "bua") {
+            return "YOU LOSE";
+        } else if (action[turn] == "bao") {
+            return "YOU WIN";
+        } else {
+            return "DRAW";
+        } 
+    }
+
+    if(yourAction == "bua"){
+        if(action[turn] == "keo") {
+            return "YOU WIN";
+        } else if (action[turn] == "bua") {
+            return "YOU LOSE";
+        } else {
+            return "DRAW";
+        }
+    }
+    
+    if(yourAction == "bao"){
+        if(action[turn] == "keo") {
+            return "YOU LOSE";
+        } else if (action[turn] == "bua") {
+            return "YOU WIN";
+        } else {
+            return "DRAW";
+        }
+    }
+
+}
+console.log(keoBuaBao("keo"));
