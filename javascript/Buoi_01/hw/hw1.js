7ff0aaf47ac0269849ca59f82b4ceac95b8f0eac
Lai Duy Nghia <laiduynghia21@gmail.com>
2022-07-09 15:06:47 +0700

    update

--- javascript/Buoi_01/hw/hw1.js
+++ javascript/Buoi_01/hw/hw1.js
@@ -1,35 +1,17 @@
-let products = [
-  {
-    name: "Iphone 13 Pro Max", // Tên sản phẩm
-    price: 3000000, // Giá mỗi sản phẩm
-    brand: "Apple", // Thương hiệu
-    count: 2, // Số lượng sản phẩm trong giỏ hàng
-  },
-  {
-    name: "Samsung Galaxy Z Fold3",
-    price: 41000000,
-    brand: "Samsung",
-    count: 1,
-  },
-  {
-    name: "IPhone 11",
-    price: 15500000,
-    brand: "Apple",
-    count: 1,
-  },
-  {
-    name: "OPPO Find X3 Pro",
-    price: 19500000,
-    brand: "OPPO",
-    count: 3,
-  },
-];
-// 6. Thêm 1 sản phẩm bất kỳ vào trong mảng product
+function calculateFactorial(num) {
+	if (num = 1) {
+        return 1;
+    }
+    return num * calculateFactorial(num - 1);
+}
+console.log(calculateFactorial(3));
 
-// 7. Xóa tất cả sản phẩm của thương hiệu "Samsung" trong giỏ hàng
-
-// 8. Sắp xếp giỏ hàng theo price tăng dần
-
-// 9. Sắp xếp giỏ hàng theo count giảm dần
-
-// 10. Lấy ra 2 sản phẩm bất kỳ trong giỏ hàng
+// Câu 2:
+function reverseStringname(text) {
+    let reverseString = "" ;
+    for (let i = text.length - 1; i >= 0; i--) {
+        reverseString += text[i];
+    }
+    return reverseString;
+}
+console.log(reverseStringname("hello"));
